9ff3e51a0255e709c72399b7abd3969577cb59d0
MORK-RAKSA <mork.raksa.1221@rupp.edu.kh>
2025-07-31 10:49:33 +0700

    Sample Microservice: setup multi-module Spring Boot microservices with Eureka, Gateway, Core, ...

--- core/src/main/java/com/raksa/app/CoreApplication.java
+++ core/src/main/java/com/raksa/app/CoreApplication.java
@@ -2,12 +2,10 @@ package com.raksa.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class CoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(CoreApplication.class, args);
     }
 }
---
+++ core/src/main/java/com/raksa/app/response/BaseResponse.java
@@ -0,0 +1,4 @@
+package com.raksa.app.response;
+
+public class BaseResponse {
+}
